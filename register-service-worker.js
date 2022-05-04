d333c4ee00df30bf8ae05141d6ff823d080ff49e
Erisa Sala <erisasala7@gmail.com>
2022-05-04 14:14:14 +0200

    vapidkeys

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,13 +1,13 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('https://aghilanbaskar.github.io/pwa-demo/service-worker.js');
+        navigator.serviceWorker.register('https://erisasala7.github.io/pwa-demo/service-worker.js');
     });
 }
 
 if ('Notification' in window && Notification.permission != 'granted') {
     console.log('Ask user permission')
-    Notification.requestPermission(status => {  
-        console.log('Status:'+status)
+    Notification.requestPermission(status => {
+        console.log('Status:' + status)
         displayNotification('Notification Enabled');
     });
 }
@@ -19,15 +19,15 @@ const displayNotification = notificationTitle => {
         navigator.serviceWorker.getRegistration().then(reg => {
             console.log(reg)
             const options = {
-                    body: 'Thanks for allowing push notification !',
-                    icon:  '/assets/icons/icon-512x512.png',
-                    vibrate: [100, 50, 100],
-                    data: {
-                      dateOfArrival: Date.now(),
-                      primaryKey: 0
-                    }
-                  };
-    
+                body: 'Thanks for allowing push notification !',
+                icon: '/assets/icons/icon-512x512.png',
+                vibrate: [100, 50, 100],
+                data: {
+                    dateOfArrival: Date.now(),
+                    primaryKey: 0
+                }
+            };
+
             reg.showNotification(notificationTitle, options);
         });
     }
@@ -36,28 +36,28 @@ const displayNotification = notificationTitle => {
 const updateSubscriptionOnYourServer = subscription => {
     console.log('Write your ajax code here to save the user subscription in your DB', subscription);
     console.log(JSON.stringify(subscription))
-    // write your ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
+        // write your ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
 };
-
-const subscribeUser = async () => {
+const vapidPKey = webPush.generateVAPIDKeys();
+const subscribeUser = async() => {
     const swRegistration = await navigator.serviceWorker.getRegistration();
-    const applicationServerPublicKey = 'BOcTIipY07N4Y63Y-9r7NMoJHofmCzn3Pu9g-LMsgIMGH4HVr42_LW9ia0lMr68TsTLKS3UcdkE3IcC52hJDYsY'; // paste your webpush certificate public key
+    const applicationServerPublicKey = vapidPKey.publicKey; // paste your webpush certificate public key
     const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
     swRegistration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey
-    })
-    .then((subscription) => {
-        console.log('User is subscribed newly:', subscription);
-        updateSubscriptionOnYourServer(subscription);
-    })
-    .catch((err) => {
-        if (Notification.permission === 'denied') {
-          console.warn('Permission for notifications was denied')
-        } else {
-          console.error('Failed to subscribe the user: ', err)
-        }
-    });
+            userVisibleOnly: true,
+            applicationServerKey
+        })
+        .then((subscription) => {
+            console.log('User is subscribed newly:', subscription);
+            updateSubscriptionOnYourServer(subscription);
+        })
+        .catch((err) => {
+            if (Notification.permission === 'denied') {
+                console.warn('Permission for notifications was denied')
+            } else {
+                console.error('Failed to subscribe the user: ', err)
+            }
+        });
 };
 const urlB64ToUint8Array = (base64String) => {
     const padding = '='.repeat((4 - base64String.length % 4) % 4)
@@ -74,22 +74,22 @@ const urlB64ToUint8Array = (base64String) => {
     return outputArray;
 };
 
-const checkSubscription = async () => {
+const checkSubscription = async() => {
     const swRegistration = await navigator.serviceWorker.getRegistration();
-    if(!swRegistration){
+    if (!swRegistration) {
         console.log('No service worker detected')
         return;
     }
     swRegistration.pushManager.getSubscription()
-    .then(subscription => {
-        if (!!subscription) {
-            console.log('User IS Already subscribed.');
-            updateSubscriptionOnYourServer(subscription);
-        } else {
-            console.log('User is NOT subscribed. Subscribe user newly');
-            subscribeUser();
-        }
-    });
+        .then(subscription => {
+            if (!!subscription) {
+                console.log('User IS Already subscribed.');
+                updateSubscriptionOnYourServer(subscription);
+            } else {
+                console.log('User is NOT subscribed. Subscribe user newly');
+                subscribeUser();
+            }
+        });
 };
 
 checkSubscription();
