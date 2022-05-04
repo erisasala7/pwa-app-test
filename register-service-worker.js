ea0caf63fb02763f922c4e39a537ce8af1e51bf2
Erisa Sala <erisasala7@gmail.com>
2022-05-04 15:03:03 +0200

    testt

--- register-service-worker.js
+++ register-service-worker.js
@@ -3,41 +3,8 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('https://erisasala7.github.io/pwa-app-test/service-worker.js');
     });
 }
-navigator.serviceWorker.ready
-    .then(function(registration) {
-        // Use the PushManager to get the user's subscription to the push service.
-        return registration.pushManager.getSubscription()
-            .then(async function(subscription) {
-                // If a subscription was found, return it.
-                if (subscription) {
-                    return subscription;
-                }
 
-                // Get the server's public key
-                const vapidPublicKey = await window.vapidPublicKey.text();
-                // Chrome doesn't accept the base64-encoded (string) vapidPublicKey yet
-                // urlBase64ToUint8Array() is defined in /tools.js
-                const convertedVapidKey = urlBase64ToUint8Array(window.vapidPublicKey);
 
-                // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
-                // send notifications that don't have a visible effect for the user).
-                return registration.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: convertedVapidKey
-                });
-            });
-    }).then(function(subscription) {
-        // Send the subscription details to the server using the Fetch API.
-        fetch('./register', {
-            method: 'post',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                subscription: subscription
-            }),
-        });
-    });
 
 
 
