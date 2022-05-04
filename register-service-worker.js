21454fc5e1b6c5eb8c4a772397df9a419bbb15d5
Erisa Sala <erisasala7@gmail.com>
2022-05-04 16:13:48 +0200

    c

--- register-service-worker.js
+++ register-service-worker.js
@@ -265,7 +265,9 @@ const subscribeUser = async() => {
             }
         });
 };
-const urlB64ToUint8Array = (base64String) => {
+
+function urlB64ToUint8Array(base64String) {
+
     const padding = '='.repeat((4 - base64String.length % 4) % 4)
     const base64 = (base64String + padding)
         .replace(/\-/g, '+')
