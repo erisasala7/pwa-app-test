6f32e229c3a5b6dc2d84fa76102943d74eaa050e
Erisa Sala <erisasala7@gmail.com>
2022-05-04 18:21:59 +0200

    bf

--- node.js
+++ node.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const webpush = require('web-push');
-
-const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
-const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
-
-// Replace with your email
-webpush.setVapidDetails('mailto:val@karpov.io', publicVapidKey, privateVapidKey);
-
+const pushSubscription = { "endpoint": "https://fcm.googleapis.com/fcm/send/fS7h7qz21ZQ:AP…RcKBLKEcWjeHcYRxADg2k-GB6jaG560MuNmawEe_n1N8jnV55", "keys": { "auth": "qX6AMD5JWbu41cFWE3Lk8w", "p256dh": "BLxHw0IMtBMzOHnXgPxxMgSYXxwzJPxpgR8KmAbMMe1-eOudcIcUTVw0QvrC5gWOhZs-yzDa4yKooqSnM3rnx7Y" } };
+//your web certificates public-key
+const vapidPublicKey = 'BFF4a8X89ZTfWGhzPSncasOkOpyAJxKzWfVXzX-BT2R7-E8GJaCvGwEDnXXJYs0Lxo7pF_xaLDftZQhZUGmFaX4';
+//your web certificates private-key
+const vapidPrivateKey = 'aCz8WZ-wIGdSNZnsH9quvVmExg9abUT1JWysMV4oJPI';
 const app = express();
 
 app.use(require('body-parser').json());
@@ -17,12 +13,46 @@ app.post('/subscribe', (req, res) => {
     const payload = JSON.stringify({ title: 'test' });
 
     console.log(subscription);
-    console.log("subscription");
+
     webpush.sendNotification(subscription, payload).catch(error => {
         console.error(error.stack);
     });
 });
+var payload = JSON.stringify({
+    "options": {
+        "body": "PWA push notification testing fom backend",
+        "badge": "/assets/icon/icon-152x152.png",
+        "icon": "/assets/icon/icon-152x152.png",
+        "vibrate": [100, 50, 100],
+        "data": {
+            "id": "458",
+        },
+        "actions": [{
+            "action": "view",
+            "title": "View"
+        }, {
+            "action": "close",
+            "title": "Close"
+        }]
+    },
+    "header": "Notification from Geekflare-PWA Demo"
+});
 
-app.use(require('express-static')('./'));
+var options = {
+    vapidDetails: {
+        subject: 'mailto:your-actual-mail@gmail.com',
+        publicKey: vapidPublicKey,
+        privateKey: vapidPrivateKey
+    },
+    TTL: 60
+};
 
-app.listen(3000);
+webPush.sendNotification(
+    pushSubscription,
+    payload,
+    options
+).then(data => {
+    return res.json({ status: true, message: 'Notification sent' });
+}).catch(err => {
+    return res.json({ status: false, message: err });
+});
--- register-service-worker.js
+++ register-service-worker.js
@@ -1,124 +1,80 @@
-const publicVapidKey = 'BFF4a8X89ZTfWGhzPSncasOkOpyAJxKzWfVXzX-BT2R7-E8GJaCvGwEDnXXJYs0Lxo7pF_xaLDftZQhZUGmFaX4';
-
 if ('serviceWorker' in navigator) {
-    console.log('Registering service worker');
-
-    run().catch(error => console.error(error));
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('https://erisasala7.github.io/pwa-app-test/service-worker.js');
+        displayNotification('Notifications')
+    });
 }
 
-async function run() {
-    console.log('Registering service worker');
-    const registration = await navigator.serviceWorker.
-    register('https://erisasala7.github.io/pwa-app-test/service-worker.js');
-    console.log('Registered service worker');
 
-    console.log('Registering push');
-    const subscription = await registration.pushManager.
-    subscribe({
-        userVisibleOnly: true,
-        // The `urlBase64ToUint8Array()` function is the same as in
-        // https://www.npmjs.com/package/web-push#using-vapid-key-for-applicationserverkey
-        applicationServerKey: urlB64ToUint8Array(publicVapidKey)
-    });
-    console.log('Registered push');
-
-    console.log('Sending push');
-    await fetch('/subscribe', {
-        method: 'POST',
-        body: JSON.stringify(subscription),
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
-    console.log('Sent push');
-}
-console.log('Loaded service worker!');
-
-self.addEventListener('push', ev => {
-    const data = ev.data.json();
-    console.log('Got push', data);
-    self.registration.showNotification(data.title, {
-        body: 'Hello, World!',
-        icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
-    });
+Notification.requestPermission(status => {
+    console.log('Status:' + status)
+    displayNotification('Notification Enabled');
 });
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//         navigator.serviceWorker.register('https://erisasala7.github.io/pwa-app-test/service-worker.js');
-//     });
-// }
 
 
-// Notification.requestPermission(status => {
-//     console.log('Status:' + status)
-//     displayNotification('Notification Enabled');
-// });
+const displayNotification = notificationTitle => {
+    console.log('display notification')
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(reg => {
+            console.log(reg)
+            const options = {
+                body: 'Thanks for allowing push notification !',
+
+                vibrate: [100, 50, 100],
+                data: {
+                    dateOfArrival: Date.now(),
+                    primaryKey: 0
+                }
+            };
+
+            reg.showNotification(notificationTitle, options);
+        });
+    }
+};
+
+const updateSubscriptionOnYourServer = subscription => {
+    console.log('Write your ajax code here to save the user subscription in your DB', subscription);
+    // write your own ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
+};
+
+const subscribeUser = async() => {
+    const swRegistration = await navigator.serviceWorker.getRegistration();
+    const applicationServerPublicKey = 'BFF4a8X89ZTfWGhzPSncasOkOpyAJxKzWfVXzX-BT2R7-E8GJaCvGwEDnXXJYs0Lxo7pF_xaLDftZQhZUGmFaX4'; // paste your webpush certificate public key
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey
+        })
+        .then((subscription) => {
+            console.log('User is subscribed newly:', subscription);
+            updateSubscriptionOnYourServer(subscription);
+        })
+        .catch((err) => {
+            if (Notification.permission === 'denied') {
+                console.warn('Permission for notifications was denied')
+            } else {
+                console.error('Failed to subscribe the user: ', err)
+            }
+        });
+};
 
 
+const checkSubscription = async() => {
+    const swRegistration = await navigator.serviceWorker.getRegistration();
+    swRegistration.pushManager.getSubscription()
+        .then(subscription => {
+            if (!!subscription) {
+                console.log('User IS Already subscribed.');
+                updateSubscriptionOnYourServer(subscription);
+            } else {
+                console.log('User is NOT subscribed. Subscribe user newly');
+                subscribeUser();
+            }
+        });
+};
 
-// const displayNotification = notificationTitle => {
-//     console.log('display notification')
-//     if (Notification.permission == 'granted') {
-//         navigator.serviceWorker.getRegistration().then(reg => {
-//             console.log(reg)
-//             const options = {
-//                 body: 'Thanks for allowing push notification !',
-
-//                 vibrate: [100, 50, 100],
-//                 data: {
-//                     dateOfArrival: Date.now(),
-//                     primaryKey: 0
-//                 }
-//             };
-
-//             reg.showNotification(notificationTitle, options);
-//         });
-//     }
-// };
-
-// const updateSubscriptionOnYourServer = subscription => {
-//     console.log('Write your ajax code here to save the user subscription in your DB', subscription);
-//     // write your own ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
-// };
-
-// const subscribeUser = async() => {
-//     const swRegistration = await navigator.serviceWorker.getRegistration();
-//     const applicationServerPublicKey = 'BFF4a8X89ZTfWGhzPSncasOkOpyAJxKzWfVXzX-BT2R7-E8GJaCvGwEDnXXJYs0Lxo7pF_xaLDftZQhZUGmFaX4'; // paste your webpush certificate public key
-//     const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
-//     swRegistration.pushManager.subscribe({
-//             userVisibleOnly: true,
-//             applicationServerKey
-//         })
-//         .then((subscription) => {
-//             console.log('User is subscribed newly:', subscription);
-//             updateSubscriptionOnYourServer(subscription);
-//         })
-//         .catch((err) => {
-//             if (Notification.permission === 'denied') {
-//                 console.warn('Permission for notifications was denied')
-//             } else {
-//                 console.error('Failed to subscribe the user: ', err)
-//             }
-//         });
-// };
-
-
-// const checkSubscription = async() => {
-//     const swRegistration = await navigator.serviceWorker.getRegistration();
-//     swRegistration.pushManager.getSubscription()
-//         .then(subscription => {
-//             if (!!subscription) {
-//                 console.log('User IS Already subscribed.');
-//                 updateSubscriptionOnYourServer(subscription);
-//             } else {
-//                 console.log('User is NOT subscribed. Subscribe user newly');
-//                 subscribeUser();
-//             }
-//         });
-// };
-
-// checkSubscription();
+checkSubscription();
 
 
 const urlB64ToUint8Array = (base64String) => {
