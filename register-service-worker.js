ab23251492d6fcc43f51aca1e5471dfba96538bb
Erisa Sala <erisasala7@gmail.com>
2022-05-04 16:51:45 +0200

    bugf

--- register-service-worker.js
+++ register-service-worker.js
@@ -10,7 +10,7 @@ async function send() {
         userVisibleOnly: true,
 
         //public vapid key
-        applicationServerKey: urlBase64ToUint8Array(publicVK)
+        applicationServerKey: urlB64ToUint8Array(publicVK)
     });
 
     //Send push notification
