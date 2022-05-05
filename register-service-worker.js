27e5d32cad8021dc39902343389884221129a4ca
Erisa Sala <erisasala7@gmail.com>
2022-05-05 10:42:12 +0200

    table fix

--- register-service-worker.js
+++ register-service-worker.js
@@ -8,22 +8,23 @@ if ('serviceWorker' in navigator) {
 
 
 Notification.requestPermission(status => {
-    let myTable = document.querySelector('#table');
-    let headers = ['Datum', 'Uhrzeit', 'Fehlermeldung'];
-    let table = document.createElement('table');
-    let headerRow = document.createElement('tr');
-    headers.forEach(headerText => {
-        let header = document.createElement('th');
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-    table.appendChild(headerRow);
+
     if (status != 'granted') {
         alert("Sie haben die Banachrichtigungen nicht zugelassen");
     } else if (status == "granted") {
 
         this.setInterval(() => {
+            let myTable = document.querySelector('#table');
+            let headers = ['Datum', 'Uhrzeit', 'Fehlermeldung'];
+            let table = document.createElement('table');
+            let headerRow = document.createElement('tr');
+            headers.forEach(headerText => {
+                let header = document.createElement('th');
+                let textNode = document.createTextNode(headerText);
+                header.appendChild(textNode);
+                headerRow.appendChild(header);
+            });
+            table.appendChild(headerRow);
             var today = new Date();
             var date;
             var dateTime;
