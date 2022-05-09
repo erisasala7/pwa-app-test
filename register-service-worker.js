27df3a780f3f378a569b90867c6f0fa5491336e0
Erisa Sala <erisasala7@gmail.com>
2022-05-09 15:55:47 +0200

    test

--- register-service-worker.js
+++ register-service-worker.js
@@ -174,6 +174,8 @@
 //     }
 //     return outputArray;
 // };
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 MsgElem = document.getElementById('msg');
 TokenElem = document.getElementById('token');
 NotisElem = document.getElementById('notis');
